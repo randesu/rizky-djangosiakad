de64a8a7319d01e2d49ee1dc429ea08a3f92f7e6
randesu <androinimesubs@gmail.com>
2024-12-04 20:11:01 +0700

    update model class

--- akademik/admin.py
+++ akademik/admin.py
@@ -10,16 +10,24 @@ from .models import (
 
 @admin.register(TahunAkademik)
 class TahunAkademikAdmin(ModelAdmin):
+    list_display = ['tahun', 'semester']
     pass
 
 @admin.register(MataKuliah)
 class MataKuliahAdmin(ModelAdmin):
+    list_display = ['nama', 'kode', 'sks','semester']
+
     pass
 
 @admin.register(Jadwal)
 class JadwalAdmin(ModelAdmin):
+    list_display = ['dosen', 'matakuliah', 'hari','jam_mulai', 'jam_selesai',
+                    'semester', 'kuota_peserta', 'tahun_akademik']
+
     pass
 
 @admin.register(KRS)
 class KRSAdmin(ModelAdmin):
+    list_display = ['tahun_akademik', 'mahasiswa', 'jadwal']
+
     pass
--- akademik/models.py
+++ akademik/models.py
@@ -6,34 +6,69 @@ class TahunAkademik(models.Model):
     tahun = models.IntegerField(null=True)
     # semester
     semester = models.IntegerField(null=True)
+    def __str__(self):
+        return str(self.semester)
     pass
 
 
 # Create your models here.
 class MataKuliah(models.Model):
     # nama
+    nama = models.CharField(blank=False, max_length=100)
+    
     # kode
+    kode = models.IntegerField(blank=False)
+    
     # sks
+    sks = models.IntegerField(blank=False)
+
     # semester
+    semester = models.ForeignKey(TahunAkademik, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.nama)
     pass
 
 
 class Jadwal(models.Model):
     # dosen
+    dosen = models.CharField(blank=False, max_length=100)
+
     # mata_kuliah
+    matakuliah = models.ForeignKey(MataKuliah, on_delete=models.CASCADE)
+
     # hari 
+    hari = models.CharField(blank=False)
+
     # jam_mulai
+    jam_mulai = models.TimeField()
+
     # jam_selesai
+    jam_selesai = models.TimeField() 
+
     # semester
-    # ruang
+    semester = models.IntegerField(blank=False)
+    
     # kuota_peserta
+    kuota_peserta = models.IntegerField(blank=False)
+
     # tahun_akademik
+    tahun_akademik = models.IntegerField(blank=False)
+
+    def __str__(self):
+        return str(self.tahun_akademik)
+
     pass
 
 
 class KRS(models.Model):
     # tahun_akademik
+    tahun_akademik = models.ForeignKey(Jadwal, on_delete=models.CASCADE)
+
     # mahasiswa
+    mahasiswa = models.CharField(blank=False, max_length=100)
+
     # jadwal
+    jadwal = models.ForeignKey(MataKuliah, on_delete=models.CASCADE)
     
     pass
